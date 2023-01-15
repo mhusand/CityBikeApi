316369726da32b18bef9df387e826da90008c9b9
Mari Husand <mari.husand@gmail.com>
2023-01-15 18:16:04 +0100

    legger til kommentar for autodiscovery

--- CityBikeApi/Services/ApiPath.cs
+++ CityBikeApi/Services/ApiPath.cs
@@ -1,6 +1,7 @@
 ﻿namespace CityBikeApi.Services
 {
     public class ApiPath   {
+        //TODO: Next version could also add autodiscovery to https://gbfs.urbansharing.com/oslobysykkel.no/gbfs.json for better version handling.
         public static string StationInformation = $"station_information.json";
         public static string StationStatus = $"station_status.json";
     }
